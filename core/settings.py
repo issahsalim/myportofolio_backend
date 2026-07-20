95e9f15fcb4073d2a494d19ccbdd3733cdf73a62
issah salim <issahsalim233@gmail.com>
2026-07-20 15:04:26 +0000

    Fix MEDIA_URL leading slash and serve media/static files unconditionally

--- core/settings.py
+++ core/settings.py
@@ -133,11 +133,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # CORS configuration
 CORS_ALLOW_ALL_ORIGINS = True
--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.static import serve
 from .views import home_view, custom_404_view
 
 # Custom 404 handler
@@ -12,10 +11,8 @@ urlpatterns = [
     path('', home_view, name='home'),
     path('admin/', admin.site.urls),
     path('api/', include('portfolio.urls')),
-    
-    # Production Media Files Serving
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve media & static files in production
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
